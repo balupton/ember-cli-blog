933e6aa826cd0800b4be629de577c4f995e77ccc
Benjamin Lupton <b@lupton.cc>
2014-07-24 15:20:05 +1000

    Got post route going with http://stackoverflow.com/a/19190529/130638

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,6 +1,6 @@
 import Ember from "ember";
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.findBy('id', params.post_id);
+    return this.store.find('post', params.post_id);
   }
 });
