775f024975fb53ebb333a167a7839fa80229cfb7
Martin Broerse <broerse@martinic.com>
2014-08-28 17:44:17 +0200

    Remove test data from Fortune server

--- server/server.js
+++ server/server.js
@@ -5,19 +5,10 @@ var fortune = require('fortune')
   .resource('post', {
     title: String,
     author: String,
-	date: Date,
-	excerpt: String,
-	body: String
-  })
-  .resource('test', {
-    title: String
+	  date: Date,
+	  excerpt: String,
+	  body: String
   })
   .listen(1337);
   
-  // {"posts": [{
-   // "title": "String2",
-   // "author": "author2",
-   // "date": "2012-04-23T18:25:43.511Z",
-   // "excerpt": "excerpt2",
-   // "body": "body2"
-  // }]}
+ 
