4ebb4fb00eb4797a31b01805d36b6b48aa7f1e69
Martin Broerse <broerse@martinic.com>
2014-09-06 09:38:19 +0200

    Change NULL to empty string in createRecord

--- client/app/controllers/posts.js
+++ client/app/controllers/posts.js
@@ -3,7 +3,7 @@ import Ember from "ember";
 export default Ember.ObjectController.extend({
   actions: {
     createPost: function() {
-      var newPost = this.get('store').createRecord('post', this.get('post'));
+      var newPost = this.get('store').createRecord('post');
       newPost.set('date' , new Date());
       newPost.set('author' , 'C.L.I. Ember');
       this.get('target').transitionTo('post', newPost.save());
--- client/app/models/post.js
+++ client/app/models/post.js
@@ -1,11 +1,11 @@
 import DS from "ember-data";
 
 var Post = DS.Model.extend({
-	title: DS.attr('string'),
-	author: DS.attr('string'),
+	title: DS.attr('string', {defaultValue: ""}),
+	author: DS.attr('string', {defaultValue: ""}),
 	date: DS.attr('date'),
-	excerpt: DS.attr('string'),
-	body: DS.attr('string')
+	excerpt: DS.attr('string', {defaultValue: ""}),
+	body: DS.attr('string', {defaultValue: ""})
 });
 
 export default Post;
