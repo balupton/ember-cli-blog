29518978416e674beb0b9c3fa86267caf12c4bb8
Martin Broerse <broerse@martinic.com>
2014-09-07 22:53:41 +0200

    sync to couchDB

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,11 @@
-// import DS from "ember-data";
+// You can enable CORS in CouchDB using:
+//
+// $ npm install -g add-cors-to-couchdb
+// $ add-cors-to-couchdb
+
+var db = new PouchDB('bloggr');
+db.sync('http://localhost:5984/bloggr', {live: true});
 
 export default EmberPouch.Adapter.extend({
-  db: new PouchDB('MyappDB')
+  db: db
 });
