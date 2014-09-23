4eaa9d18c0e384f9979a95b018f68ed1360a8981
Martin Broerse <broerse@martinic.com>
2014-09-23 21:57:58 +0200

    Add timeout 20 sec for slow iriscouch

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,4 @@
-var db = new PouchDB('bloggr');
+var db = new PouchDB('bloggr', {ajax: {timeout: 20000}});
 db.sync('http://martinic.iriscouch.com/bloggr', {live: true});
 
 export default EmberPouch.Adapter.extend({
