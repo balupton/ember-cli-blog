61d806c6e10c8f7a00162e11c26699cff0a67928
Martin Broerse <broerse@martinic.com>
2014-09-25 13:07:44 +0200

    set 'edit' on createPost

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -3,6 +3,7 @@ import Ember from "ember";
 export default Ember.ObjectController.extend({
   actions: {
     createPost: function() {
+      this.controllerFor('post').send('edit');
       var newPost = this.get('store').createRecord('post');
       newPost.set('date' , new Date());
       newPost.set('author' , 'C.L.I. Ember');
