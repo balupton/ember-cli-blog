45d91fcad13ccb9445ec21550fb03befe29eee37
Martin Broerse <broerse@martinic.com>
2014-10-10 22:15:59 +0200

    Merge back

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 var db = new PouchDB('bloggr');
-var remote = new PouchDB('http://martinic.iriscouch.com/bloggr', {ajax: {timeout: 20000}});
+var remote = new PouchDB('http://localhost:5984/bloggr', {ajax: {timeout: 20000}});
 db.sync(remote, {live: true});
 
 export default EmberPouch.Adapter.extend({
