49334b811eef488bd20f3ccf5744e9dc0da0b151
Martin Broerse <broerse@martinic.com>
2014-10-16 18:25:16 +0200

    Moved actions in routes

--- app/routes/post.js
+++ app/routes/post.js
@@ -3,22 +3,5 @@ import Ember from "ember";
 export default Ember.Route.extend({
 	model: function(params) {
 		return this.get('store').find('post', params.post_id);
-	},
-	
-	actions: {
-	  edit: function() {
-			this.controller.set('isEditing', true);
-		},
-
-		doneEditing: function() {
-			this.controller.set('isEditing', false);
-			this.get('controller.model').save();
-		},
-		
-		deletePost: function() {
-      this.get('controller.model').destroyRecord().then(function() {
-        this.transitionTo('posts');
-      }.bind(this));
-    }
-  }
+	}
 });
--- app/routes/posts.js
+++ app/routes/posts.js
@@ -17,12 +17,28 @@ export default Ember.Route.extend({
   },
   
   actions: {
+	  edit: function() {
+			this.controllerFor('post').set('isEditing', true);
+		},
+
+		doneEditing: function() {
+			this.controllerFor('post').set('isEditing', false);
+			this.modelFor('post').save();
+		},
+		
+		deletePost: function() {
+      this.modelFor('post').destroyRecord().then(function() {
+        this.transitionTo('posts');
+      }.bind(this));
+    },
+
     createPost: function() {
-      this.controllerFor('post').set('isEditing', true);
+      this.send('edit');
       var newPost = this.get('store').createRecord('post');
       newPost.set('date' , new Date());
       newPost.set('author' , 'C.L.I. Ember');
       this.transitionTo('post', newPost.save());
     }
-  },
+  }
+  
 });
