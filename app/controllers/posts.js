025429493aaafa2842b1a38fb796767eafda2e8d
Martin Broerse <broerse@martinic.com>
2014-10-22 21:47:27 +0200

    Add the new pagination with PagedLocalArray

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,6 +1,9 @@
 import Ember from "ember";
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.ArrayController.extend({
   sortProperties: ['date'],
-  sortAscending: false
+  sortAscending: false,
+  
+  pagedContent: pagedArray('content', {perPage: 5}),
 });
