ceea1d5e6681c822bfef4f4d4cc5c427d59dbd1c
Martin Broerse <broerse@martinic.com>
2015-01-17 14:20:59 +0100

    Remove console.logs

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,5 @@
 import config from '../config/environment';
 
-console.log(config.local_couchdb_instance);
-console.log(config.baseURL);
-
 var db = new PouchDB(config.local_couchdb_instance);
 var remote = new PouchDB(config.online_couchdb_instance, {ajax: {timeout: 20000}});
 db.sync(remote, {live: true});
