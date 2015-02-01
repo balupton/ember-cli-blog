e69bdb812ecaeaddf8a51a6171e734b0627f29ea
Martin Broerse <broerse@martinic.com>
2015-02-01 16:28:23 +0100

    Add Filter Posts

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -8,9 +8,22 @@ export default Ember.ArrayController.extend({
   page: 1,
   perPage: 5,
 
-  pagedContent: pagedArray("arrangedContent", {pageBinding: "page", perPageBinding: "perPage"}),
+  pagedContent: pagedArray("filteredContent", {pageBinding: "page", perPageBinding: "perPage"}),
 
-  queryParams: ["page", "perPage"],
+  queryParams: ["page", "perPage", "query"],
 
-  totalPagesBinding: "pagedContent.totalPages"
+  totalPagesBinding: "pagedContent.totalPages",
+  
+  filteredContent: function() {
+    if (this.get('query')) {
+      return this.get('arrangedContent').filter(function(item) {
+        var query = this.get('query').toLowerCase(),
+            name = (item.get('title') || '').toLowerCase(),
+            desc = (item.get('body') || '').toLowerCase();
+
+        return name.match(query) || desc.match(query);
+      }.bind(this));
+    }
+    return this.get('arrangedContent');
+  }.property('arrangedContent', 'query')
 });
