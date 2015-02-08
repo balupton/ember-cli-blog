b864a32ddeaf68b8d13dd30babc70de31096e3eb
Martin Broerse <broerse@martinic.com>
2015-02-08 13:06:16 +0100

    Fix iriscouch sync

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,16 @@ import config from '../config/environment';
 
 var db = new PouchDB(config.local_couch || 'bloggr');
 var remote = new PouchDB(config.remote_couch, {ajax: {timeout: 20000}});
-db.sync(remote, {live: true});
+
+function doSync() {
+  db.sync(remote, {live: true})
+    .on('error', function() {
+      // Retry connection every 5 seconds
+      setTimeout(doSync, 5000);
+    });
+}
+
+doSync();
 
 export default EmberPouch.Adapter.extend({
   db: db
