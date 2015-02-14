d0bdb752a9d21c6749b1d1f66f1ed1372ac21f07
Martin Broerse <broerse@martinic.com>
2015-02-14 19:05:49 +0100

    Add case insensitive sorting

--- app/controllers/authors.js
+++ app/controllers/authors.js
@@ -4,6 +4,18 @@ import pagedArray from 'ember-cli-pagination/computed/paged-array';
 export default Ember.ArrayController.extend({
   sortProperties: ['name'],
   sortAscending: true,
+  sortFunction: function(v, w) {
+    var lowerV = v.toLowerCase();
+    var lowerW = w.toLowerCase();
+
+    if (lowerV < lowerW) {
+      return -1;
+    }
+    if (lowerV > lowerW) {
+      return 1;
+    }
+    return 0;
+  },
   
   page: 1,
   perPage: 5,
