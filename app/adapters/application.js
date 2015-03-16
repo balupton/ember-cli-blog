3dddb2b5bb94446e915614220517dc0537217a93
Nolan Lawson <nolan.lawson@gmail.com>
2015-03-16 11:46:22 -0400

    update ember-pouch and pouchdb

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,15 +3,7 @@ import config from '../config/environment';
 var db = new PouchDB(config.local_couch || 'bloggr');
 var remote = new PouchDB(config.remote_couch, {ajax: {timeout: 20000}});
 
-function doSync() {
-  db.sync(remote, {live: true})
-    .on('error', function() {
-      // Retry connection every 5 seconds
-      setTimeout(doSync, 5000);
-    });
-}
-
-doSync();
+db.sync(remote, {live: true, retry: true});
 
 export default EmberPouch.Adapter.extend({
   db: db
