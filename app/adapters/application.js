8f41be20d415a67dad7605ba0ef2f116acb9238c
Rhett Sutphin <rhett@detailedbalance.net>
2015-03-26 08:19:59 +0100

    Add change watcher for ember-data

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,5 +6,20 @@ var remote = new PouchDB(config.remote_couch, {ajax: {timeout: 20000}});
 db.sync(remote, {live: true, retry: true});
 
 export default EmberPouch.Adapter.extend({
-  db: db
+  db: db,
+  
+  // Change watcher for ember-data
+  immediatelyLoadAllChangedRecords: function() {
+    this.db.changes({
+      since: 'now',
+      live: true,
+      returnDocs: false
+    }).on('change', function (change) {
+      var obj = this.db.rel.parseDocID(change.id);
+      // skip changes for non-relational_pouch docs. E.g., design docs.
+      if (!obj.type || obj.type === '') { return; }
+      var store = this.container.lookup('store:main');
+      store.find(obj.type);
+    }.bind(this));
+  }.on('init')
 });
