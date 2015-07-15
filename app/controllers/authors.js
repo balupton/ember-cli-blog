e2dd7779d0769430d54bf81807410342382e8740
Martin Broerse <broerse@martinic.com>
2015-07-15 22:50:18 +0200

    Remove ArrayController for authors

--- app/controllers/authors.js
+++ app/controllers/authors.js
@@ -1,22 +1,7 @@
 import Ember from "ember";
 import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
-export default Ember.ArrayController.extend({
-  sortProperties: ['name'],
-  sortAscending: true,
-  sortFunction: function(v, w) {
-    var lowerV = v.toLowerCase();
-    var lowerW = w.toLowerCase();
-
-    if (lowerV < lowerW) {
-      return -1;
-    }
-    if (lowerV > lowerW) {
-      return 1;
-    }
-    return 0;
-  },
-  
+export default Ember.Controller.extend({
   page: 1,
   perPage: 5,
 
@@ -24,5 +9,25 @@ export default Ember.ArrayController.extend({
 
   queryParams: ["page", "perPage"],
 
-  totalPagesBinding: "pagedContent.totalPages"
+  totalPagesBinding: "pagedContent.totalPages",
+
+  arrangedContent: function() {
+    return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+      sortProperties: ['name'],
+      sortAscending: true,
+      sortFunction: function(v, w) {
+        var lowerV = v.toLowerCase();
+        var lowerW = w.toLowerCase();
+
+        if (lowerV < lowerW) {
+          return -1;
+        }
+        if (lowerV > lowerW) {
+          return 1;
+        }
+        return 0;
+      },
+      content: this.get('model')
+    });
+  }.property('model')
 });
