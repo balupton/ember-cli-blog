81f7efae2292e954d2c6abe4d2bf5f88eb1ce9f6
Martin Broerse <broerse@martinic.com>
2015-07-19 11:58:46 +0200

    Add Ember Data 2.0 Reload behavior

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -9,7 +9,13 @@ db.sync(remote, {live: true, retry: true});
 
 export default Adapter.extend({
   db: db,
-  
+
+  // Ember Data 2.0 Reload behavior
+  shouldReloadRecord: function() { return true; },
+  shouldReloadAll: function() { return true; },
+  shouldBackgroundReloadRecord: function() { return true; },
+  shouldBackgroundReloadAll: function() { return true; },
+
   // Change watcher for ember-data
   immediatelyLoadAllChangedRecords: function() {
     this.db.changes({
@@ -20,10 +26,10 @@ export default Adapter.extend({
       var obj = this.db.rel.parseDocID(change.id);
       // skip changes for non-relational_pouch docs. E.g., design docs.
       if (!obj.type || obj.type === '') { return; }
-      
+
       var appController = this.container.lookup("controller:application");
       appController.send('kickSpin');
-      
+
       var store = this.container.lookup('store:main');
       store.find(obj.type);
     }.bind(this));
