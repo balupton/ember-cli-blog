0a9b57e590a2eaac47d85d84e5f7a244c40e86b6
Martin Broerse <broerse@martinic.com>
2015-07-19 08:26:32 +0200

    this.store.find => this.store.findRecord

--- app/routes/author.js
+++ app/routes/author.js
@@ -2,6 +2,6 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
 	model: function(params) {
-		return this.store.find('author', params.author_id);
+		return this.store.findRecord('author', params.author_id);
 	}
 });
