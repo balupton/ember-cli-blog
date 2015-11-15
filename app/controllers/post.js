7f8c4e13a4ff0e4d2795171e8d2553acf509669c
Martin Broerse <broerse@martinic.com>
2015-11-15 14:33:59 +0100

    Change Needs: comment

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -3,7 +3,7 @@ import Ember from "ember";
 export default Ember.Controller.extend({
   posts: Ember.inject.controller(),
   authors: function () {
-    // we can access authors on the posts controller thanks to the needs declaration above
+    // we can access authors on the posts controller thanks to the Ember.inject.controller above
     return this.get('posts.authors');
   }.property("posts.authors.@each.name")
 });
