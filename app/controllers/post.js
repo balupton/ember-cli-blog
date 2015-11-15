616705ba2b0ac810007d31f7b11c441177b2bdec
Martin Broerse <broerse@martinic.com>
2015-11-15 14:30:40 +0100

    Fix Needs: deprecation

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,9 +1,9 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
-  needs: "posts",
+  posts: Ember.inject.controller(),
   authors: function () {
     // we can access authors on the posts controller thanks to the needs declaration above
-    return this.get('controllers.posts.authors');
-  }.property("controllers.posts.authors.@each.name")
+    return this.get('posts.authors');
+  }.property("posts.authors.@each.name")
 });
