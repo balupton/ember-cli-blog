3fd109a1e13238127b6799f1a0031ed93d618b7b
Martin Broerse <broerse@martinic.com>
2015-11-19 15:53:09 +0100

    Optimize posts.authors

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,9 +1,5 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
-  posts: Ember.inject.controller(),
-  authors: function () {
-    // we can access authors on the posts controller thanks to the Ember.inject.controller above
-    return this.get('posts.authors');
-  }.property("posts.authors.@each.name")
+  posts: Ember.inject.controller()
 });
