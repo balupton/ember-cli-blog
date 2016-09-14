d3cdb74d2df032ef5c56d7cc91431ea02a12c52f
Joren l'Ami <joren@distinct.nl>
2016-09-14 10:42:00 +0200

    ember-simple-auth-pouch beta2 update

--- app/authenticators/pouch.js
+++ app/authenticators/pouch.js
@@ -1,13 +1,9 @@
 import Pouch from 'ember-simple-auth-pouch/authenticators/pouch';
-import Ember from 'ember';
-const { getOwner } = Ember;
 
 export default Pouch.extend({
-	init() {
-	    this._super(...arguments);
-		
+	getDb() {
 	    let pouchAdapter = this.get('store').adapterFor('application');//getOwner(this).lookup(`adapter:${pouchAdapterName}`);
 
-		this.db = pouchAdapter.remoteDb;
+		return pouchAdapter.remoteDb;
 	}
 });
