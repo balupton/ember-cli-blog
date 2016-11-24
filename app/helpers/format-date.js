b8e697e957f0af2c470da320036ca1860a1e7867
Martin Broerse <broerse@martinic.com>
2016-11-24 10:03:31 +0100

    Move moment.js from bower to npm

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,4 +1,5 @@
 import Ember from "ember";
+import moment from 'moment';
 
 export default Ember.Helper.helper(function(params) {
 	let value = params[0];
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,7 +21,6 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
 
   app.import('bower_components/marked/lib/marked.js');
-  app.import('bower_components/moment/moment.js');
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 
   return app.toTree();
