e93c5bb5178ce179704967f41f92c612225ba1c0
Martin Broerse <broerse@martinic.com>
2016-11-26 14:47:04 +0100

    Switch to the bootstrap npm package

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,7 +20,5 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-
   return app.toTree();
 };
