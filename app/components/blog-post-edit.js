2c74b88865d538f960977026f803f1119e6cc445
Martin Broerse <broerse@martinic.com>
2016-12-15 13:59:31 +0100

    Change author to belongsTo in post model

--- app/components/blog-post-edit.js
+++ app/components/blog-post-edit.js
@@ -1,24 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  authorlist: function() {
-    var selected = this.get('post.author'); // author from post model
-    var content = [];
-    if (selected !== null) {
-      content.push(selected);
-    }
-
-    this.get('authors').forEach(function(listAuthorObj) {
-      var listName = listAuthorObj.get('name');
-      if (selected !== listName) {
-        content.push(listName);
-      }
-    });
-
-    return content.sort();
-  }.property("post.author", "authors"),
-
-
   actions: {
     edit: function() {
       this.set('isEditing', true);
--- app/models/post.js
+++ app/models/post.js
@@ -3,7 +3,7 @@ import { Model } from 'ember-pouch';
 
 var Post = Model.extend({
 	title: DS.attr('string', {defaultValue: ""}),
-	author: DS.attr('string', {defaultValue: ""}),
+	author: DS.belongsTo('author'),
 	date: DS.attr('date'),
 	excerpt: DS.attr('string', {defaultValue: ""}),
 	body: DS.attr('string', {defaultValue: ""})
